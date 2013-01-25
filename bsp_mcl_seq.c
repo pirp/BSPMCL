1e335484a9b695d30068188fae7f2a38243b36c9
Heliosmaster <helios89@gmail.com>
2013-01-25 19:28:50 +0100

    something works

--- bsp_mcl_seq.c
+++ bsp_mcl_seq.c
@@ -3,35 +3,81 @@
 #include <Mondriaan.h>
 
 void sparse_mult(struct sparsematrix m1, struct sparsematrix m2) {
-	printf("%ld,%ld",m1.NrNzElts,m2.NrNzElts);
+	int i,j,k,r,s;
+	int row, col,row2,col2;
+	double** val;
+	val = (double**) malloc(m1.n*sizeof(double*));
+	for(i=0;i<m1.n;i++) val[i] = (double*) malloc(m1.n*sizeof(double));
+	
+	for(i=0;i<m1.n;i++) for(j=0;j<m1.n;j++) val[i][j] =0;
+	for(k=0;k<m1.NrNzElts;k++){
+		row = m1.i[k];
+		col = m1.j[k];
+		for(r=0;r<m2.NrNzElts;r++){
+			row2 = m2.i[r];
+			col2 = m2.j[r];
+			if(row2 == col){
+				val[row][col2] += m1.ReValue[k]*m2.ReValue[r];
+			}
+		}
+	}
+	/*
+	for(i=0;i<m1.n;i++){
+		for(j=0;j<m1.n;j++) printf("%1.0f ",val[i][j]);
+		printf("\n");
+	}
+	*/
+	printf("\n\n\n");
+
+	int count =0;
+	for(i=0;i<m1.n;i++) for(j=0;j<m1.n;j++) if(val[i][j] != 0) count++;
+	//printf("nonzeros=%d\n\n",count);
+	
+
+	//int* rows,cols,vals;
+	int* rows = (int*) malloc(count*sizeof(int));
+	int* cols = (int*) malloc(count*sizeof(int));
+	double* vals = (double*) malloc(count*sizeof(double));
+
+	k=0;
+	for(i=0;i<m1.n;i++) for(j=0;j<m1.n;j++){
+		if(val[i][j]!=0){
+			rows[k] = i;
+			cols[k] = j;
+			vals[k] = val[i][j];
+			k++;
+		}
+	}
+	/*
+	for(k=0;k<count;k++){
+		printf("(%d,%d)=%f\n",rows[k]+1,cols[k]+1,vals[k]);
+	}
+*/
+	for(i=0;i<m1.n;i++) free(val[i]);
+	free(val);
 }
 
 
 int main(int argc, char **argv){
-	FILE *File;
-	/* This structure will contain the testMatrix matrix. */
+	FILE* File;
 	struct sparsematrix testMatrix;
 
-	/* Open the testMatrix matrix file. */
-	if (!(File = fopen("test_matrix.mtx", "r")))
+	if (!(File = fopen("bcsstk08.mtx", "r")))
 	{
 		printf("Unable to open test_matrix!\n");
 		return EXIT_FAILURE;
 	}
 	
-	/* Read it from the file. */
 	if (!MMReadSparseMatrix(File, &testMatrix))
 	{
 		printf("Unable to read test_matrix!\n");
 		fclose(File);
 		return EXIT_FAILURE;
 	}
-	fclose(File);
-	int j=0;
-	long int nonzero = testMatrix.NrNzElts;
-	for(j=0;j<nonzero;j++) printf("%d: (%ld,%ld)=%1.0f\n",j,testMatrix.i[j]+1,testMatrix.j[j]+1,testMatrix.ReValue[j]);
-	printf("\n");
+
 	sparse_mult(testMatrix,testMatrix);
+	
+	fclose(File);
 }
 
 
